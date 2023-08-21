26c7cc68ebe6a3ac8de884da7a99a38a59fa367e
Kenny Frempong <brakojo21@gmail.com>
2023-08-21 07:28:42 -0700

    Function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,14 +1,5 @@
 #include "main.h"
 
-/**
- * _putchar - Writes a character to stdout.
- * @c: The character to write.
- * Return: 1 on success, -1 on error.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
 
 /**
  * _printf - a function that produces output according to a format.
