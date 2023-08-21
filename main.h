f96ae1f76afd5a30de95f9a7bdc7c8196adb01b2
Kenny Frempong <brakojo21@gmail.com>
2023-08-21 01:51:54 -0700

    Function that produces output according to a format.

--- main.h
+++ main.h
@@ -4,9 +4,12 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _puts(char *c);
+int __printf(const char *format, ...);
 
 #endif
